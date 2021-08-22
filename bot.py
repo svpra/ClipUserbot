846bdc80e88f10f223a0052d18ab25ebe8e6e428
Артур <68541852+A9FM@users.noreply.github.com>
2021-08-22 23:41:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -128,7 +128,7 @@ with alive_bar(24, bar='classic2', title='Подготовка', spinner='pointe
             pip.main(["install", "simpledemotivators"])
         except:
             pip.main(["install", "wheel"])
-            os.system("pkg install libjpeg-turbo libfreetype6-dev -y")
+            os.system("pkg install libjpeg-turbo -y")
             os.system("LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install Pillow")
             pip.main(["install", "simpledemotivators"])
 
