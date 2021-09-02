ba80369fb21f19bb6bf8c5f6fe29d527ab6c4343
Артур <68541852+A9FM@users.noreply.github.com>
2021-09-02 11:54:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -247,7 +247,7 @@ async def help(client: Client, message: Message):
         telegraph = Telegraph()
         telegraph.create_account(short_name='ClipUserbot')
         help = """
-<p align="center"><a href="https://t.me/arturdestroyerbot"><img src="https://github.com/A9FM/filesUB/blob/main/logo.png" width="500"></a></p> 
+<a href="https://github.com/A9FM/filesUB/blob/main/logo.png" >.</a>
 <b><a href="https://t.me/ArturDestroyerBot">🤖 UserBot CLIP 1.9.4 (бета) 🤖</a></b><br>
 <b><a href="https://t.me/artur_destroyer">👨💻 Создатель 👨💻</a></b><br>
 <b><a href="https://www.donationalerts.com/r/a9fm">💰 Донат Создателю 💰</a></b><br>
